e229d78be3c765474542a13d4e65bf2904cee38e
Adam Hite <adamchristopherhite@gmail.com>
2019-05-11 19:34:52 -0500

    custom club template (needs more work)

---
+++ wp-content/themes/NAH/single-wpsl_stores.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Example of a single WPSL store template for the Twenty Fifteen theme.
+ * 
+ * @package Twenty_Fifteen
+ */
+
+get_header(); ?>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+               <header class="entry-header">
+                    <h1 class="entry-title"><?php single_post_title(); ?></h1>        
+                </header>
+                <div class="entry-content">
+                <?php
+                    global $post;
+
+                    $queried_object = get_queried_object();
+                    
+                    // Add the map shortcode
+                    echo do_shortcode( '[wpsl_map]' );
+                    
+                    // Add the content
+                    $post = get_post( $queried_object->ID );
+                    setup_postdata( $post );
+                    wp_reset_postdata( $post );
+                    
+                    // Add the address shortcode
+                    echo do_shortcode( '[wpsl_address]' );
+
+                    the_content();
+                ?>
+                </div>
+            </article>
+        </main><!-- #main -->
+    </div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
